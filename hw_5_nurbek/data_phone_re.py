3836462e6efc3f95c8170de6ad9b9b5d085ba1b9
Nurbek <nurbekkalilov@gmail.com>
2021-06-19 14:04:52 +0600

    Updated homework for lesson 5

--- hw_5_nurbek/data_phone_re.py
+++ hw_5_nurbek/data_phone_re.py
@@ -14,5 +14,5 @@ print(f"Total amount of phone numbers with 13 digits: {len(found)}")
 phone_number1 = r"[(][0-6][0-9][0-9][)]\s\d{3}-\d{4}"
 found1 = re.findall(phone_number1, text)
 # print(found1)
-print(f'Total amount of phone numbers with 13 digits: {len(found1)}')
+print(f'Total amount of phone которые имеют формат (669) 106-9923: {len(found1)}')
 
